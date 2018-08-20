3bb7fa2df0d28dfb02134cf30faca071daa885a7
Isara Naranirattisai <isaradream@gmail.com>
2018-08-20 23:21:03 +0700

    implement: Milestone 4

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -1,9 +1,16 @@
 package observatory
 
+import scala.collection.mutable
+import scala.collection.parallel.ParIterable
+
 /**
   * 4th milestone: value-added information
   */
 object Manipulation {
+
+  val memoGridTemperature = new mutable.HashMap[(Int, Int), Temperature]()
+  val memoAvgGridTemperature = new mutable.HashMap[(Int, Int), Temperature]()
+  val memoDevGridTemperature = new mutable.HashMap[(Int, Int), Temperature]()
 
   /**
     * @param temperatures Known temperatures
@@ -11,16 +18,24 @@ object Manipulation {
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    grid:GridLocation => {
+      memoGridTemperature.getOrElseUpdate((grid.lat, grid.lon),
+        Visualization.predictTemperature(temperatures, Location(grid.lat, grid.lon)))
+    }
   }
 
   /**
-    * @param temperaturess Sequence of known temperatures over the years (each element of the collection
+    * @param temperaturess Sequence of known temperatures over the years (each element of th     e collection
     *                      is a collection of pairs of location and temperature)
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+    grid:GridLocation => {
+      memoAvgGridTemperature.getOrElseUpdate((grid.lat, grid.lon), {
+        val temperaturesList: ParIterable[Temperature] = temperaturess.par.map(temperatures => makeGrid(temperatures)(grid))
+        temperaturesList.sum/temperaturesList.size
+      })
+    }
   }
 
   /**
@@ -29,7 +44,11 @@ object Manipulation {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+    grid:GridLocation => {
+      memoDevGridTemperature.getOrElseUpdate((grid.lat, grid.lon), {
+        makeGrid(temperatures)(grid) - normals(grid)
+      })
+    }
   }
 
 
